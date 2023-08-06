2718241482feb719db7cb4ec5e0e54c84df22de2
ehddk <bj9147@naver.com>
2023-08-06 23:27:26 +0900

    Feat: 라우터 기능 추가

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,23 +1,17 @@
 import styled from "styled-components";
-
+import { useRouter } from "next/navigation";
+//import { Link } from "react-router-dom";
 const NavParent = styled.div`
   flex-grow: 1;
-  margin-left: 248px;
+
   height: 70px;
   box-shadow: 0 4px 0.5px 0 rgba(0, 0, 0, 0.07);
   background-color: rgba(236, 234, 254, 0.47);
   display: flex;
   align-items: center;
-<<<<<<< HEAD
-=======
-  height: 10vh;
-
-  padding: 0 20px;
-  margin-left: 248px;
->>>>>>> 2784e142205d70ac8aa47a210c58150334d912bf
 `;
 const NavTitle = styled.h1`
-  margin-left: 610px;
+  margin-left: 45%;
 `;
 const NavUser = styled.span`
   color: blue;
@@ -25,11 +19,20 @@ const NavUser = styled.span`
 `;
 
 const Nav = () => {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push("/Login");
+  };
+  // const handleTitle = () => {
+  //   router.push("/");
+  // };
+
   return (
     <>
       <NavParent>
         <NavTitle>Prama Math</NavTitle>
-        <NavUser>김하늘님</NavUser>
+        <NavUser onClick={handleLogin}>김하늘님</NavUser>
       </NavParent>
     </>
   );
