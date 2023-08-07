c8aa1e0502a90304829c799db5cfd40181c861af
ehddk <bj9147@naver.com>
2023-08-07 23:56:42 +0900

    Feat: 페이지 이동 기능 부여, css 변경

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,7 +1,9 @@
 import { useRouter } from "next/navigation";
 import styled from "styled-components";
-import SidebarItems from "./SidebarItems";
-
+//import SidebarItems from "./SidebarItems";
+import { useState } from "react";
+import { MdPerson, MdCalendarMonth, MdLogout } from "react-icons/md";
+import { usePathname } from "next/navigation";
 const SidebarParent = styled.div`
   margin: 1px 4px 0 0;
   position: fixed;
@@ -25,37 +27,58 @@ const Title = styled.h2`
 `;
 const Menu = styled.div`
   margin-top: 70px;
-  padding: 0 85px 116px 100px;
+  margin: 100px 20px 0 40px;
+`;
+const List = styled.div`
+  display: flex;
+  padding: 30px 29px 0 47px;
+  text-align: center;
 `;
 
-const StyledSidebarItem = styled.p`
-  margin-bottom: 40px;
+const Link = styled.p`
+  font-size: 20px;
+  height: 20px;
+  border: 0;
+  background: rgba(0, 0, 0, 0);
+  color: ${(props) => (props.active ? "blue" : "black")};
   cursor: pointer;
 `;
 
-const Logout = styled.div`
-  position: absolute;
-  top: 591px;
-  padding: 0 85px 116px 100px;
-  font-family: Poppins;
-  font-size: 14px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: left;
-  color: #655f5f;
+const StyledSidebarItem = styled.li`
+  margin: 10px 0 30px 0;
+  margin-left: -20px;
+  cursor: pointer;
+  list-style: none;
 `;
+
+const Logout = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  padding: 0 85px 116px 60px;
+  font-size: 14px;
+  color: #655f5f;
+  gap: 10px;
+`;
+const MainMenu = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 25px;
+`;
+const SubMenu = styled.ul`
+  margin-left: 30px;
+`;
+
 const Sidebar = () => {
   const router = useRouter();
+  const pathname = usePathname();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const handleMenuClick = (id) => {
-    if (id === 1) {
-      router.push("/schedule");
-    }
+  const handleMenuClick = (route) => {
+    router.push(route);
+    setSidebarOpen(false); // 메뉴 클릭 시 사이드바가 자동으로 닫히도록 설정
   };
-
   return (
     <>
       <SidebarParent>
@@ -63,22 +86,63 @@ const Sidebar = () => {
           <img src="" alt="logo"></img>
           <Title>AcaTech</Title>
         </Logo>
+
         <Menu>
-          {SidebarItems.map((i, id) => (
-            <StyledSidebarItem key={id} onClick={() => handleMenuClick(id)}>
-              {i.name}
+          <MainMenu>
+            <MdPerson size="25" />
+            <Link
+              href="/login"
+              active={pathname === "/Login"}
+              onClick={() => handleMenuClick("/Login")}
+            >
+              원생 관리
+            </Link>
+          </MainMenu>
+
+          <SubMenu>
+            <StyledSidebarItem onClick={() => handleMenuClick("/attendance")}>
+              -출결 관리
             </StyledSidebarItem>
-          ))}
+            <StyledSidebarItem
+              onClick={() => handleMenuClick("/student-management")}
+            >
+              -학생 관리
+              <ul>
+                <StyledSidebarItem
+                  onClick={() => handleMenuClick("/enrolled-students")}
+                >
+                  수강생
+                </StyledSidebarItem>
+                <StyledSidebarItem
+                  onClick={() => handleMenuClick("/new-counseling")}
+                >
+                  신규 상담
+                </StyledSidebarItem>
+              </ul>
+            </StyledSidebarItem>
+          </SubMenu>
+          <MainMenu>
+            <MdCalendarMonth size="25" />
+            <Link href="/login">일정 관리</Link>
+          </MainMenu>
+          <SubMenu>
+            <StyledSidebarItem
+              onClick={() => handleMenuClick("/ScheduleManagement/schedule")}
+            >
+              -전체 일정
+            </StyledSidebarItem>
+            <StyledSidebarItem
+              onClick={() => handleMenuClick("/ScheduleManagement/register")}
+            >
+              -일정 등록
+            </StyledSidebarItem>
+          </SubMenu>
         </Menu>
+
         <Logout>
+          <MdLogout size="20" />
           <p>로그아웃</p>
         </Logout>
-        {/* <SidebarItem>
-          <p>원생관리</p>
-        </SidebarItem>
-        <SidebarItem>
-          <p>스케줄 관리</p>
-        </SidebarItem> */}
       </SidebarParent>
     </>
   );
