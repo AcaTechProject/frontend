8a3d51efec31f2f83645648e909257cd96538f76
ehddk <bj9147@naver.com>
2023-08-20 00:49:37 +0900

    test: 상태관리 recoil test

---
+++ app/recoil/atom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const telState = atom({
+  key: "telState",
+  default: "",
+});
+
+export const parentState = atom({
+  key: "parentState",
+  default: "",
+});
