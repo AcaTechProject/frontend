271ca04a80bd362f1254689c8100b260d7fd0fa4
ehddk <bj9147@naver.com>
2023-08-20 21:47:58 +0900

    init: Add 헤더 컴포넌트 생성

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,34 +1,62 @@
 import styled from "styled-components";
+import { useRouter } from "next/navigation";
+import { Link } from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 const NavParent = styled.div`
-  background: #eceafe78;
+  flex-grow: 1;
+  height: 70px;
+  box-shadow: 0 3px 0.5px 0 rgba(0, 0, 0, 0.05);
+  background-color: rgba(236, 234, 254, 0.47);
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  height: 73px;
-
-  padding: 0 20px;
-  margin-left: 248px;
+  width: 100%;
+  position: fixed;
 `;
 const NavTitle = styled.h1`
-  margin-left: 615px;
+  flex-grow: 1;
+  margin-left: 45%;
+  padding: 20px 0 0 0;
+  position: relative;
 `;
-const NavUser = styled.p`
+const NavUser = styled.span`
   color: blue;
-  margin-left: auto; 
-}
-
-
+  cursor: pointer;
+  padding: 10px 0 0 0;
+`;
+const Container = styled.div`
+  display: flex;
+  margin: 10px 130px 0 0;
+  gap: 10px;
 `;
 
 const Nav = () => {
+  const router = useRouter();
+  const [img, setImg] = useState("");
+
+  const handleLogin = () => {
+    router.push("/Login");
+  };
+
   return (
-    <div>
+    <>
       <NavParent>
-        <NavTitle>Prama Math</NavTitle>
-        <NavUser>김하늘님</NavUser>
+        <NavTitle onClick={() => router.push("/")}>Prama Math</NavTitle>
+        <Container>
+          <Image
+            src={img ? img : `/default_profile.png`}
+            alt="프로필"
+            width={50}
+            height={50}
+            style={{ borderRadius: "50%" }}
+            onClick={() => router.push("/Mypage")}
+          />
+
+          <NavUser onClick={handleLogin}>김하늘님</NavUser>
+        </Container>
       </NavParent>
-    </div>
+    </>
   );
 };
 export default Nav;
