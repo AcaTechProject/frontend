f547bacd382081d8deb699139c0462c00ece97ad
ehddk <bj9147@naver.com>
2023-08-25 23:13:54 +0900

    rename: Move 폴더 위치 변경

---
+++ recoil/atom.js
@@ -0,0 +1,59 @@
+import { atom } from "recoil";
+
+export const telState = atom({
+  key: "telState",
+  default: "",
+});
+export const numState = atom({
+  key: "numState",
+  default: "",
+});
+export const emailState = atom({
+  key: "emailState",
+  default: "",
+});
+
+export const parentState = atom({
+  key: "parentState",
+  default: "",
+});
+
+export const valueState = atom({
+  key: "valueState",
+  default: [],
+});
+export const resultState = atom({
+  key: "resultState",
+  default: [],
+});
+
+export const inputAtom = atom({
+  key: "inputAtom",
+  default: "",
+});
+
+export const subjectState = atom({
+  key: "subjectState",
+  defalut: "",
+});
+export const subjectStateA = atom({
+  key: "subjectStateA",
+  defalut: "",
+});
+export const teacherState = atom({
+  key: "teacherState",
+  defalut: "",
+});
+export const textState = atom({
+  key: "textState",
+  defalut: "",
+});
+export const contentState = atom({
+  key: "contentState",
+  defalut: "",
+});
+
+export const familyState = atom({
+  key: "familyState",
+  default: "",
+});
