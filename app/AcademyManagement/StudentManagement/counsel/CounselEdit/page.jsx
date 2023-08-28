c23c65ee3ed744b5a6ae77e62121f1bb5bab9a6b
ehddk <bj9147@naver.com>
2023-08-28 23:55:06 +0900

    feat: Add 개인상담 수정 관리 페이지 생성 및 상태관리 추가

--- app/AcademyManagement/StudentManagement/counsel/CounselEdit/page.jsx
+++ app/AcademyManagement/StudentManagement/counsel/CounselEdit/page.jsx
@@ -1,57 +1,140 @@
 "use client";
-import Button from "@/app/components/Button";
+import React from "react";
+import ProfileEmpty from "@/app/components/ProfileEmpty";
 import styled from "styled-components";
-import { useState } from "react";
-import StudentList from "@/app/components/StudentList";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { useState, useEffect } from "react";
+
+import { useRouter } from "next/navigation";
+import LongSelect from "@/app/components/LongSelect";
 import Modal from "@/app/components/Modal";
+import Table from "@/app/components/Table";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  selectedSubState,
+  selectedDaeState,
+  sangdamState,
+  daesangState,
+} from "@/recoil/atom";
+
+import Button from "@/app/components/Button";
 const Container = styled.div`
   padding: 116px 70px 55px 85px;
+`;
+// const Container = styled.div`
+//   padding: 116px 70px 55px 85px;
+// `;
+
+const Body = styled.section`
+  display: flex;
+`;
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 110px 0 0 20px;
+  width: 45%;
+`;
+const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: -50px;
+  gap: 40px;
+  width: 48%;
 `;
 const Row = styled.div`
   display: flex;
   gap: 23px;
 `;
-const CustomButton = styled(Button)`
-  .MuiSvgIcon-root {
-    vertical-align: middle;
-  }
+const Row2 = styled(Row)`
+  justify-content: flex-end;
+  margin-top: 70px;
+`;
+
+const Textarea = styled.textarea`
+  width: 600px;
+  height: 200px;
 `;
 
 const CounselEdit = () => {
-  const data = [{ 상담과목: "국어", 일시: "2월", 담당교사: "김은진" }];
-  const headers = ["No", "상담과목", "일시", "담당교사"];
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalMessage, setModalMessage] = useState(false);
+  const router = useRouter();
 
-  const handleModal = (message) => {
-    setIsModalOpen(true);
-    setModalMessage(message);
+  const [changeSub, setChangeSub] = useRecoilState(selectedSubState);
+  const [changeDae, setChangeDae] = useRecoilState(selectedDaeState);
+  const selectSubject = useRecoilValue(sangdamState);
+  const selectDaesang = useRecoilValue(daesangState);
+
+  //PageRegister 페이지에서 daesangState를 사용했듯이
+  const setSangdam = useSetRecoilState(sangdamState); // setSangdam 추가
+  const setDaesang = useSetRecoilState(daesangState);
+
+  const handleSub = (e) => {
+    const newValue = e.target.value;
+    setChangeSub(newValue);
+    // 이전 페이지에서 사용한 sangdamState도 업데이트
+    setSangdam(newValue);
   };
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const handleDae = (e) => {
+    const newValue = e.target.value;
+    setChangeDae(newValue);
+    // 이전 페이지에서 사용한 daesangState도 업데이트
+    setDaesang(newValue);
+  };
+  const handleCancel = () => {
+    alert("수정이 취소되었습니다");
+    router.push("/AcademyManagement/StudentManagement/counsel/CounselDetail");
   };
 
+  const handleComplete = () => {
+    alert("수정이 완료되었습니다");
+    router.push("/AcademyManagement/StudentManagement/counsel/CounselDetail");
+  };
+
+  useEffect(() => {}, []);
   return (
     <Container>
       <p>
-        원생관리 {">"} 학생관리 {">"} 수강생 관리 {">"} 이름
+        원생관리 {">"} 학생관리 {">"} 수강생 관리 {">"} 이름 {">"} 상담관리{" "}
+        {">"} 상담등록
       </p>
-      <h2>김지수 학생 상담내역</h2>
-      <Row>
-        <CustomButton
-          $medium
-          $disabled
-          onClick={() =>
-            handleModal("선택된 학생의 상담내역을 삭제하시겠습니까?")
-          }
-        >
-          <DeleteForeverIcon size={10} />
-          전체 삭제
-        </CustomButton>
-        {isModalOpen && <Modal onClose={closeModal} message={modalMessage} />}
-      </Row>
-      <StudentList data={data} headers={headers} />
+
+      <Body>
+        <Left>
+          <ProfileEmpty />
+        </Left>
+        <Right>
+          <Row2>
+            <Button $medium $primary onClick={handleCancel}>
+              수정취소
+            </Button>
+
+            <Button $medium $primary onClick={handleComplete}>
+              수정완료
+            </Button>
+          </Row2>
+          <p>상담 과목</p>
+          <LongSelect
+            options={[
+              { value: "none", label: "상담 과목을 선택해주세요" },
+              { value: "국어", label: "국어" },
+              { value: "영어", label: "영어" },
+              { value: "수학", label: "수학" },
+            ]}
+            value={selectSubject}
+            onChange={handleSub}
+          />
+          <p>상담 대상</p>
+          <LongSelect
+            options={[
+              { value: "non", label: "상담 대상을 선택해주세요" },
+              { value: "학생", label: "학생" },
+              { value: "학부모", label: "학부모" },
+            ]}
+            value={selectDaesang}
+            onChange={handleDae}
+          />
+          <p>상담 내용</p>
+          <Textarea />
+        </Right>
+      </Body>
     </Container>
   );
 };
