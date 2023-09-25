f2da0a496519668b7e8c460f73892bb8c40c2d94
ehddk <bj9147@naver.com>
2023-09-25 12:04:43 +0900

    feat: Add 학생개인 상담내역 관리  api 연결

--- app/AcademyManagement/StudentManagement/counsel/HistoryManage/page.jsx
+++ app/AcademyManagement/StudentManagement/counsel/HistoryManage/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 import Button from "@/app/components/Button";
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import StudentList from "@/app/components/StudentList";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import Modal from "@/app/components/Modal";
+import axios from "axios";
 const Container = styled.div`
   padding: 116px 70px 55px 85px;
 `;
@@ -19,10 +20,11 @@ const CustomButton = styled(Button)`
 `;
 
 const HistoryManage = () => {
-  const data = [{ 상담과목: "국어", 일시: "2월", 담당교사: "김은진" }];
   const headers = ["No", "상담과목", "일시", "담당교사"];
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalMessage, setModalMessage] = useState(false);
+  const [userData, setUserData] = useState({});
+  const [consultInfo, setConsulInfo] = useState([]);
 
   const handleModal = (message) => {
     setIsModalOpen(true);
@@ -32,12 +34,44 @@ const HistoryManage = () => {
     setIsModalOpen(false);
   };
 
+  const data = consultInfo.map((item) => ({
+    상담과목: item.con_class,
+    일시: item.con_date,
+    담당교사: item.con_teacher,
+  }));
+
+  const url = window.location.href;
+  const urlParts = url.replace("?id=", "");
+  const studentId = urlParts[urlParts.length - 1];
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/student/${studentId}`)
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.log("오류", error);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/student/${studentId}/consulting`)
+      .then((response1) => {
+        setConsulInfo(response1.data);
+        console.log("상담 불러오기 성공!", response1.data);
+      })
+      .catch((error) => {
+        console.log("오류", error);
+      });
+  }, [studentId]);
   return (
     <Container>
       <p>
-        원생관리 {">"} 학생관리 {">"} 수강생 관리 {">"} 이름
+        원생관리 {">"} 학생관리 {">"} 수강생 관리 {">"} {userData.name} {">"}{" "}
+        상담관리 {">"} 상담내역 관리
       </p>
-      <h2>김지수 학생 상담내역</h2>
+      <h2>{userData.name} 학생 상담내역</h2>
       <Row>
         <CustomButton
           $medium
@@ -47,7 +81,7 @@ const HistoryManage = () => {
           }
         >
           <DeleteForeverIcon size={10} />
-          전체 삭제
+          삭제
         </CustomButton>
         {isModalOpen && <Modal onClose={closeModal} message={modalMessage} />}
       </Row>
