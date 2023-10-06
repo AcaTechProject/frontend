60620a5951495cc1e54969c8a4830a978cb53675
jissssu <113964353+jissssu@users.noreply.github.com>
2023-10-06 10:16:17 +0900

    feat: 개인 메세지 api 추가

--- app/components/MessagePopup.jsx
+++ app/components/MessagePopup.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
 import styled from "styled-components";
 import Modal from "./Modal";
+import { useEffect, useState } from "react";
 const PopupOverlay = styled.div`
   position: fixed;
   top: 0;
@@ -93,58 +93,123 @@ const ParentName = styled.p`
   font-size: 15px; /* 글꼴 크기 조정 */
 `;
 
-const MessagePopup = ({ onClose }) => {
+const MessagePopup = ({ onClose, studentName, messages }) => {
   const [message, setMessage] = useState("");
   const [showRecommendation, setShowRecommendation] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMessage, setSelectedMessage] = useState("");
   const [modalMessage, setModalMessage] = useState("");
+  const [recommendationText, setRecommendationText] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleModal = (message) => {
     setModalMessage(message);
     setIsModalOpen(true);
   };
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
-  const handleSendMessage = () => {
-    // 여기에 메세지 전송 로직 추가
-    console.log("Sending message:", message);
-    // 메세지 전송 후 팝업 닫기
-    onClose();
-  };
+  useEffect(() => {
+   
+    fetch("http://localhost:8080/student/message_text")
+      .then((response) => response.json())
+      .then((data) => {
+       
+        const attendanceMessages = data.filter((msg) => msg.mt_type === "출결");
+        
+        const combinedText = attendanceMessages.map((msg) => msg.mt_text).join("\n");
+        setRecommendationText(combinedText);
+      })
+      .catch((error) => {
+        console.error("API 호출 중 오류 발생:", error);
+      });
+  }, []); // 컴포넌트가 처음 렌더링될 때만 실행
 
   const handleToggleRecommendation = () => {
     setShowRecommendation(!showRecommendation);
   };
+
+  const handleSelectMessage = (selectedMessage) => {
+    setSelectedMessage(selectedMessage.replace('@@@', studentName)); // 클릭한 메시지를 저장
+    setMessage(selectedMessage.replace('@@@', studentName)); // 클릭한 메시지를 메시지 입력 창에 설정
+    // setShowRecommendation(false); 
+  };
+
+  const handleSendMessage = () => {
+    
+    const recipientPhoneNumber = "222-5678"; 
+  
+  
+    const content = message;
+  
+    
+    const messageData = {
+      recipientPhoneNumber,
+      content,
+    };
+  
+    // 메시지 발송 요청 보내기
+    fetch("http://localhost:8080/student/message/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messageData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 발송 결과 처리
+        setModalMessage(data.result); // 발송 결과 메시지 설정
+        setIsModalOpen(true); // 모달 열기
+        console.log("메시지 발송 성공:", data)
+        
+        fetch("http://localhost:8080/student/message/save", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(messageData),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            // 메시지 발송 내역 저장 결과 처리
+            console.log("메시지 발송 내역 저장 결과:", data);
+          })
+          .catch((error) => {
+            console.error("메시지 발송 내역 저장 오류:", error);
+          });
+      })
+      .catch((error) => {
+        console.error("메시지 발송 오류:", error);
+      });
+  };
+  
 
   return (
     <PopupOverlay>
       <PopupContent>
         <PopupHeader>메세지 발송</PopupHeader>
         <CloseButton onClick={onClose}>×</CloseButton>
-        <ParentName>김ㅇㅇ 학부모님</ParentName>
+        <ParentName>{studentName} 학부모님</ParentName>
         <MessageInput
           type="text"
           placeholder="메세지를 입력하세요."
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          value={message} // message 상태값 사용
+          onChange={(e) => setMessage(e.target.value)} 
         />
         <SendButton onClick={handleToggleRecommendation}>
           {showRecommendation ? "취소" : "출결"}
         </SendButton>
         {showRecommendation && (
           <RecommendationBox
-            value="*[지각알림] @@학생이 정시에 등원하지 않았습니다. 확인 부탁드립니다."
+            value={recommendationText}
             readOnly
+            onClick={() => handleSelectMessage(recommendationText)} 
           />
         )}
         {showRecommendation && (
           <NewButton onClick={() => handleModal("메시지를 발송하시겠습니까?")}>
             메세지발송
           </NewButton>
-        )}{" "}
-        {isModalOpen && <Modal onClose={closeModal} message={modalMessage} />}
+        )}
+        {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} message={modalMessage} />}
       </PopupContent>
     </PopupOverlay>
   );
