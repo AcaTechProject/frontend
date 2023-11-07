7a1684e06152bb3e7a5de3056a186ba8a035b385
ehddk <bj9147@naver.com>
2023-11-07 18:27:33 +0900

    fix: change 학생ID 파라미터

--- app/components/StudentList.jsx
+++ app/components/StudentList.jsx
@@ -1,8 +1,6 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import { useRecoilValue } from "recoil";
-
 import styled from "styled-components";
 import {
   studentNameState,
@@ -42,20 +40,11 @@ const EmptyRow = styled.tr`
 
 const StudentList = ({ data, headers, handleStudentInfo }) => {
   const router = useRouter();
-  // const [currentPage, setCurrentPage] = useState(5);
-  // const itemsPerPage = 6;
-
-  // const startIndex = (currentPage - 1) * itemsPerPage;
-  // const endIndex = startIndex + itemsPerPage;
-  // const displayedData = data.slice(startIndex, endIndex);
-
   const { push, pathname, query } = router;
+  // const studentName = useRecoilValue(studentNameState);
+  // const studentBirth = useRecoilValue(studentBirthState);
+  // const studentSchool = useRecoilValue(studentSchoolState);
   const totalItems = data.length;
-
-  const url = window.location.href;
-  const urlParts = url.replace("?id=", "");
-  const studentId = urlParts[urlParts.length - 1];
-
   if (totalItems === 0) {
     return (
       <TableContainer>
@@ -79,11 +68,9 @@ const StudentList = ({ data, headers, handleStudentInfo }) => {
 
   const onTdClick = (id) => {
     //const id = "1234";
-
     router.push(
-      `/AcademyManagement/StudentManagement/acamember/StudentInfo?id=${studentId}`
+      `/AcademyManagement/StudentManagement/acamember/StudentInfo?id=${id.id}`
     );
-
     //  console.log("id", id);
   };
 
